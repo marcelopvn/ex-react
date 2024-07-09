33d205bffa6f7a8feade4b57ec51fd8af7cc712c
Marcelo pavan <marceloluizpavan17@gmail.com>
2024-07-08 22:05:45 -0300

    finalizando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,78 @@
 import { useState } from 'react'
-import TabelaIMC from './components/Tabela'
-import Peso from './components/Peso'
-import Altura from './components/Altura'
-import CalcularIMC from './components/CalcularIMC'
-import Resultado from './components/Resultado'
+import Formulario from './components/Formulario'
+import Tabela from './components/Tabela'
+
 
 function App() {
-  const [resultado,setResultado]=useState(0)
-  const [peso,setPeso]=useState(0)
-  const [altura,setAltura]=useState(0)
+  const [IMC,setIMC] = useState('')
+  const [peso,setPeso] = useState(0)
+  const [altura,setAltura] = useState(0)
+  
+
+  const calculaIMC = () => {
+    let alturaFloat = parseFloat(altura)
+    let pesoFloat = parseFloat(peso)
+
+    if(alturaFloat > 0 && pesoFloat > 0) {
+      let alutraAoQuadrado = alturaFloat * alturaFloat
+      let imcCalculo = pesoFloat / alutraAoQuadrado
+      let imcFinal = parseFloat(imcCalculo.toFixed(2))
+      const magrezaLine = document.querySelector("#magreza");
+      const normal = document.querySelector("#normal");
+      const sobrepeso = document.querySelector("#sobrepeso");
+      const obesidade = document.querySelector("#obesidade");
+      const grave = document.querySelector("#grave");
+      magrezaLine.classList.remove('active');
+      normal.classList.remove("active");
+      sobrepeso.classList.remove("active");
+      obesidade.classList.remove("active");
+      grave.classList.remove("active");
+
+      if(imcFinal < 18.50) {
+        magrezaLine.classList.add('active')
+        return setIMC(imcFinal)
+      }
+      if(imcFinal >= 18.50 && imcFinal < 25.00) {
+        normal.classList.add('active')
+        return setIMC(imcFinal)
+      }
+      if(imcFinal >= 25.00 && imcFinal < 30.00) {
+        sobrepeso.classList.add('active')
+        return setIMC(imcFinal)
+      }
+      if(imcFinal >= 30.00 && imcFinal < 40.00) {
+        obesidade.classList.add('active')
+        return setIMC(imcFinal)
+      }
+      else{
+        grave.classList.add('active')
+        return setIMC(imcFinal)
+      }
+    }
+  }
+  const clean = () => {
+    const magrezaLine = document.querySelector("#magreza");
+      const normal = document.querySelector("#normal");
+      const sobrepeso = document.querySelector("#sobrepeso");
+      const obesidade = document.querySelector("#obesidade");
+      const grave = document.querySelector("#grave");
+      magrezaLine.classList.remove('active');
+      normal.classList.remove("active");
+      sobrepeso.classList.remove("active");
+      obesidade.classList.remove("active");
+      grave.classList.remove("active");
+    return setIMC('');
+  }
 
   return (
     <>
-    <Peso p={peso} sp={setPeso}/>
-    <Altura a={altura} sa={setAltura}/>
-    <CalcularIMC p={peso} a={altura} sr={setResultado}/>
-    <Resultado r={resultado}/>
-    <TabelaIMC/>
+      <div className="container">
+        <Formulario clean={clean} calculoIMC={calculaIMC} setAltura={e => setAltura(e.target.value)} setPeso={e => setPeso(e.target.value)}/>
+        <Tabela imc={IMC} />
+      </div>
     </>
   )
 }
+
 
 export default App
---
+++ src/components/Formulario/index.jsx
@@ -0,0 +1,24 @@
+import styles from './Formulario.module.css';
+
+const Formulario = ({setAltura, setPeso, calculoIMC, clean}) => {
+  return(
+    <form className={styles.form}>
+      <div className={styles.inputs}>
+        <div className={styles.altura__block}>
+          <label htmlFor="altura">Altura <small>(ex.: 1,70)</small></label>
+          <input type="number" id='altura' onBlur={setAltura} min={0}/>
+        </div>
+        <div className={styles.peso__block}>
+          <label htmlFor="peso">Peso <small>(ex.: 69,2)</small></label>
+          <input type="number" id='peso' onBlur={setPeso} min={0}/>
+        </div>
+      </div>
+      <div className={styles.buttons}>
+        <button onClick={clean} className={styles.clear} type="reset">Limpar <i className="bi bi-caret-right"></i></button>
+        <button onClick={calculoIMC} id='calcular' type="button">Calcular <i className="bi bi-caret-right"></i></button>
+      </div>
+    </form>
+  )
+}
+
+export default Formulario
